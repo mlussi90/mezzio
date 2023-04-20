ca3d84d2b7abac843a0400a985b47d7f149ea324
alexmerlin <alex.merlin.1985@gmail.com>
2023-04-20 10:23:19 +0300

    Issue #52: Implemented MiddlewareFactoryInterface.
    Signed-off-by: alexmerlin <alex.merlin.1985@gmail.com>

--- test/ConfigProviderTest.php
+++ test/ConfigProviderTest.php
@@ -17,6 +17,7 @@ use Mezzio\Handler\NotFoundHandler;
 use Mezzio\Middleware;
 use Mezzio\MiddlewareContainer;
 use Mezzio\MiddlewareFactory;
+use Mezzio\MiddlewareFactoryInterface;
 use Mezzio\Response\ServerRequestErrorResponseGenerator;
 use Mezzio\Router\RouterInterface;
 use PHPUnit\Framework\TestCase;
@@ -50,6 +51,8 @@ class ConfigProviderTest extends TestCase
     {
         $aliases = $this->provider->getDependencies()['aliases'] ?? [];
 
+        self::assertArrayHasKey(RequestHandlerRunnerInterface::class, $aliases);
+        self::assertArrayHasKey(MiddlewareFactoryInterface::class, $aliases);
         self::assertArrayHasKey(DEFAULT_DELEGATE, $aliases);
         self::assertArrayHasKey(DISPATCH_MIDDLEWARE, $aliases);
         self::assertArrayHasKey(IMPLICIT_HEAD_MIDDLEWARE, $aliases);
